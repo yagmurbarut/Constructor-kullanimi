236bf6342a70954158b2cf4eaa8fa0ce2a8edae4
Yağmur Barut <119848166+yagmurbarut@users.noreply.github.com>
2023-11-05 20:58:45 +0300

    Create ana-proje.java

---
+++ ana-proje.java
@@ -0,0 +1,10 @@
+public class Udemydevam {
+
+    public static void main(String[] args) {
+        HesapMakinesi h1 = new HesapMakinesi(12, 5);
+        System.out.println(h1.toplama());
+        System.out.println(h1.bolme());
+        System.out.println(h1.carpma());
+        System.out.println(h1.cikarma());
+    }
+}
